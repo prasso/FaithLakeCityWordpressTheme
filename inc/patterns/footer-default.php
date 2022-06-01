2ee830d73e43c4922b6716c36c33886eedd003d4
Bobbi <bcp@faxt.com>
2022-06-01 08:18:00 -0400

    fix header

--- inc/patterns/footer-default.php
+++ inc/patterns/footer-default.php
@@ -6,18 +6,37 @@ return array(
 	'title'      => __( 'Default footer', 'faithlakecity' ),
 	'categories' => array( 'footer' ),
 	'blockTypes' => array( 'core/template-part/footer' ),
-	'content'    => '<!-- wp:group {"align":"full","layout":{"inherit":true}} -->
-					<div class="wp-block-group alignfull"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"4rem","bottom":"4rem"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
-					<div class="wp-block-group alignwide" style="padding-top:4rem;padding-bottom:4rem"><!-- wp:site-title {"level":0} /-->
+	'content'    => '
+	<!-- wp:group {"backgroundColor":"secondary","textColor":"foreground","layout":{"inherit":true}} -->
+<div class="wp-block-group has-foreground-color has-secondary-background-color has-text-color has-background">
+  <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"0","top":"0"}}},"layout":{"type":"flex","justifyContent":"space-between","textColor":"vivid-cyan-blue"}} -->
+  <div class="wp-block-group alignwide" style="padding-top:0;padding-bottom:0">
+    <!-- wp:columns -->
+    <div class="wp-block-columns">
+      <!-- wp:column -->
+      <div class="wp-block-column">
+        <!-- wp:site-logo {"width":278,"shouldSyncIcon":false} /-->
+      </div>
+      <!-- /wp:column -->
 
-					<!-- wp:paragraph {"align":"right"} -->
-					<p class="has-text-align-right">' .
-					sprintf(
-						/* Translators: WordPress link. */
-						esc_html__( 'Proudly powered by %s', 'faithlakecity' ),
-						'<a href="' . esc_url( __( 'https://wordpress.org', 'faithlakecity' ) ) . '" rel="nofollow">WordPress</a>'
-					) . '</p>
-					<!-- /wp:paragraph --></div>
-					<!-- /wp:group --></div>
-					<!-- /wp:group -->',
+      <!-- wp:column -->
+      <div class="wp-block-column">
+        <!-- wp:html -->
+        <div>
+          <div>
+            <p style="font-weight: 700; font-size: 1.6277em;">Faith Baptist Church &nbsp;~ &nbsp;Lake City, Florida</p>
+            <p style="font-weight: 400; font-size: 1.3885em;">Visit us on Faith Road behind The Home Depot, Aldi\'s, and Ashley Furniture</p>
+            <p style="font-weight: 400; font-size: 1.3885em;"><a href="https://www.facebook.com/FaithLakeCity" data-location="external" data-detail="https://www.facebook.com/FaithLakeCity/" data-category="link" target="_blank" class="cloverlinks" rel="noopener"><span data-socialicon="roundedfacebook"><span class="socialIconSymbol" aria-hidden="true"></span><span class="sr-only">roundedfacebook</span></span></a>  &nbsp;<span data-socialicon="roundedinstagram"><span class="socialIconSymbol" aria-hidden="true"></span><span class="sr-only">roundedinstagram</span></span>  &nbsp;<span data-socialicon="roundeditunes"><span class="socialIconSymbol" aria-hidden="true"></span><span class="sr-only">roundeditunes</span></span>  &nbsp;<a href="https://www.youtube.com/channel/UChYu0xZ0ZSQC2Y8CM4jv0ow/" data-location="external" data-detail="https://www.youtube.com/channel/UChYu0xZ0ZSQC2Y8CM4jv0ow/" data-category="link" target="_blank" class="cloverlinks" rel="noopener"><span data-socialicon="roundedyoutube"><span class="socialIconSymbol" aria-hidden="true"></span><span class="sr-only">roundedyoutube</span></span></a> </p>
+          </div>
+        </div>
+        <!-- /wp:html -->
+      </div>
+      <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+  </div>
+  <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
+	',
 );
